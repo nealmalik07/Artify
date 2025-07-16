5f6a70aad63211db12eec24335b44c1ae05fbfdf
nealmalik07 <nealmalik07@gmail.com>
2025-07-16 16:35:38 +0530

    Add files via upload

---
+++ php/config.php
@@ -0,0 +1,20 @@
+<?php
+$host = 'localhost';
+$db   = '';//add the name of your database
+$user = '';//add the uesrname for the mysql database
+$pass = '';//add the password to your mysql database
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+?>
